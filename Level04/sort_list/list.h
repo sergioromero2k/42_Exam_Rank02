b49fb52bc586c173a73720d19e76f00ea7d10fdf
Sergio Alejandro <sergioromero2k@gmail.com>
2025-12-13 05:08:32 +0100

    examen preview rank02

--- Level04/sort_list/list.h
+++ Level04/sort_list/list.h
@@ -1,30 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   exam_rank02.h                                      :+:      :+:    :+:   */
+/*   list.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sergio-alejandro <sergio-alejandro@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/12/13 04:34:44 by sergio-alej       #+#    #+#             */
-/*   Updated: 2025/12/13 04:56:39 by sergio-alej      ###   ########.fr       */
+/*   Created: 2025/12/13 05:06:47 by sergio-alej       #+#    #+#             */
+/*   Updated: 2025/12/13 05:07:35 by sergio-alej      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef EXAM_RANK02_H
-# define EXAM_RANK02_H
-
-# include <stdarg.h>
-# include <stdbool.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <unistd.h>
-
-typedef struct s_list
-{
-	void			*content;
-	struct s_list	*next;
-}					t_list;
-
-t_list	*ft_lstnew(void *content);
-
-#endif
