24cb89ae41c5f8df995a0f34b46491027732f244
Sergio Alejandro <sergioromero2k@gmail.com>
2025-12-22 21:26:55 +0100

    add rev_print.c

--- Level01/rev_print.c
+++ Level01/rev_print.c
@@ -1,52 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   repeat_alpha.c                                     :+:      :+:    :+:   */
+/*   rev_print.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sergio-alejandro <sergio-alejandro@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/12/22 20:12:16 by sergio-alej       #+#    #+#             */
-/*   Updated: 2025/12/22 20:35:11 by sergio-alej      ###   ########.fr       */
+/*   Created: 2025/12/22 20:40:10 by sergio-alej       #+#    #+#             */
+/*   Updated: 2025/12/22 21:23:23 by sergio-alej      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_putchar(char c, int num)
-{
-	while (num > 0)
-	{
-		write(1, &c, 1);
-		--num;
-	}
-}
-
-void	repeat_alpha(char *str)
+void	rev_print(char *s)
 {
 	int	i;
 
 	i = 0;
-	while (str[i] != '\0')
-	{
-		if (str[i] >= 'A' && str[i] <= 'Z')
-		{
-			ft_putchar(str[i], str[i] - 'A' + 1);
-		}
-		else if (str[i] >= 'a' && str[i] <= 'z')
-		{
-			ft_putchar(str[i], str[i] - 'a' + 1);
-		}
-		else
-		{
-			ft_putchar(str[i], 1);
-		}
+	while (s[i] != '\0')
 		i++;
-	}
+	while (i--)
+		write(1, &(s[i]), 1);
 }
 int	main(int argc, char **argv)
 {
 	if (argc == 2)
-		repeat_alpha(argv[1]);
+		rev_print(argv[1]);
 	write(1, "\n", 1);
 	return (0);
 }
