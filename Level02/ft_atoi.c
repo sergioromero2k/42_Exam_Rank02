e9739fad118d4ae5f30de4326b363f39dda443c7
Sergio Alejandro <sergioromero2k@gmail.com>
2026-01-04 06:02:20 +0100

    strcmp add

--- Level02/ft_atoi.c
+++ Level02/ft_atoi.c
@@ -1,8 +1,34 @@
-int	ft_atoi(const char *str){
-    int sign;
-    int result;
-    int i;
+// #include "ctype.h"
+// #include "stdio.h"
 
-    i=0;
-    while(isspace())
+int	ft_atoi(const char *str)
+{
+	int	sign;
+	int	result;
+	int	i;
+
+	i = 0;
+	sign = 1;
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\r' || str[i] == '\n'
+		|| str[i] == '\v' || str[i] == '\f')
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	result = 0;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = (result * 10) + (str[i] - '0');
+		i++;
+	}
+	return (sign * result);
 }
+/* int	main(void)
+{
+	printf("%i\n", ft_atoi("-12153"));
+	return (0);
+}
+ */
---
+++ Level02/ft_strcspn.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+size_t	ft_strcspn(const char *s, const char *reject){
+    int i;
+
+    i=0;
+    while(s[i] != '\0' || s[i] == reject){
+
+
+    }
+}
